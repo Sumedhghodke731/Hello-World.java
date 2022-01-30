9b8e4256b2840421cac2273cf9fc20240270869f
Sumedhghodke731 <98696731+Sumedhghodke731@users.noreply.github.com>
2022-01-30 19:24:40 +0530

    Create PrintStar.java

---
+++ PrintStar.java
@@ -0,0 +1,13 @@
+  public class star {
+    public static void main (String[] agrs){
+        int i,j;
+        for (i=1;i<=4;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}       
