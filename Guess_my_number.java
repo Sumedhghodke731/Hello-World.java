272dbd656490eb26b268cdfdc1265265f6b0c546
Sumedhghodke731 <98696731+Sumedhghodke731@users.noreply.github.com>
2022-01-31 11:53:10 +0530

    Create Guess_my_number.java

---
+++ Guess_my_number.java
@@ -0,0 +1,21 @@
+package com.tes;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+            int number=0;
+        do {
+            System.out.println("input a number");
+             number = sc.nextInt();
+            System.out.print("here is your number");
+            System.out.println(number);
+        }while(number>=0);
+        System.out.println("the end");
+
+      
+       
+        
+    }
+}
